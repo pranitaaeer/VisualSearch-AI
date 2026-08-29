5252593b9ba921e3d5c0914031460f21600a23d2
pranitaaeer <aeerpranita@gmail.com>
2026-08-29 15:14:47 +0530

    indentation fixed

--- ml-service/main.py
+++ ml-service/main.py
@@ -6,7 +6,7 @@ from services.image_embedding import generate_image_embedding
 from services.similarity import cosine_similarity
 from services.vector_store import (create_collection,store_embedding,search_similar_images,get_all_images,)
 from services.image_hash import  (generate_image_hash, is_near_duplicate,)
- from services.image_classifier import classify_image
+from services.image_classifier import classify_image
 
 app = FastAPI(title="VisualSearch AI ML Service")
 
